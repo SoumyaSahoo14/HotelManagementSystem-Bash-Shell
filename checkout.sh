a1ee26c3c974688434c48c09c3705ea0b1c473fe
Soumya Sahoo <163902079+SoumyaSahoo14@users.noreply.github.com>
2025-05-24 13:17:32 +0530

    Update checkout.sh
    Price changes and bills generation during checkout.

--- checkout.sh
+++ checkout.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 echo "===== 🚪 Guest Checkout ====="
 read -p "Enter Guest ID: " guest_id
 
-# Get guest info
 guest_line=$(grep "^$guest_id," guests.csv)
 
 if [ -z "$guest_line" ]; then
@@ -13,10 +10,8 @@ fi
 
 room_no=$(echo "$guest_line" | cut -d',' -f6)
 
-# Get room type
 room_type=$(awk -F, -v r="$room_no" '$1 == r {print $2}' rooms.csv)
 
-# Set room price
 case "$room_type" in
     "Standard") price=1000 ;;
     "Deluxe") price=2000 ;;
@@ -25,20 +20,16 @@ case "$room_type" in
     *) price=1500 ;;
 esac
 
-# Remove from guests.csv
 grep -v "^$guest_id," guests.csv > temp.csv && mv temp.csv guests.csv
 
-# Remove from booked_guests.csv
 grep -v "$guest_id" booked_guests.csv > temp.csv && mv temp.csv booked_guests.csv
 
-# Update room status to Available
 awk -F, -v rn="$room_no" -v OFS=',' '
 NR==1 {print; next}
 $1 == rn {$3="Available"}
 {print}
 ' rooms.csv > temp.csv && mv temp.csv rooms.csv
 
-# Save to bills and revenue
 echo "$guest_id,$room_no,$room_type,$price" >> bills.csv
 echo "$price" >> revenue.csv
 
