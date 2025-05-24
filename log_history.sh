cd47389d918a476f9988555560d58eb22ff98189
Soumya Sahoo <163902079+SoumyaSahoo14@users.noreply.github.com>
2025-05-24 13:20:28 +0530

    Update log_history.sh
    Timestamp records for history

--- log_history.sh
+++ log_history.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
 guest_id="$1"
 action="$2"
 details="$3"
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
 
-# Get guest name
 guest_name=$(grep "^$guest_id," guests.csv | cut -d',' -f2)
 
 echo "$guest_id,$guest_name,$action,$details,$timestamp" >> history.csv
